762fa16c6bf902ef1e3180fa1a5abd363a1e0274
Diogo Santos <diogo@diogosantos.com>
2010-09-25 22:22:55 -0300

    handling mouse click on navigator view

--- Classes/WallpaperNavigatorView.rb
+++ Classes/WallpaperNavigatorView.rb
@@ -15,6 +15,22 @@ class WallpaperNavigatorView < NSView
     @wallpaper_view.display_wallpaper( next_wallpaper() )
   end
   
+  def mouseDown(sender)  
+    if wallpaper_clicked?(sender) then
+       puts "click!"
+    end    
+  end
+  
+  def wallpaper_clicked?(sender)
+    NSPointInRect(adjust_click_location(sender.locationInWindow), @wallpaper_view.bounds)
+  end
+  
+  def adjust_click_location(point)    
+    point.x = point.x - 6
+    point.y = point.y - 54
+    return point
+  end
+  
   def next_wallpaper
     return Wallpaper.wallpaper_default unless received_wallpapers?
     
